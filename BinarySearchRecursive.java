9288f309c7c19a45fbf8c53782e68308d66e371f
DilverSingh23 <dsinghh23@gmail.com>
2025-02-04 17:47:17 -0500

    Add recursive implementation of binary search

---
+++ BinarySearchRecursive.java
@@ -0,0 +1,28 @@
+public class BinarySearchRecursive {
+    public static int search(int[] nums, int target) {
+        return searchHelper(nums, target, 0, nums.length-1);
+    }
+
+    public static int searchHelper(int[] nums, int target, int l, int r) {
+        int m = (l+r)/2;
+        if (l > r) {
+            return -1;
+        }
+        if (nums[m] == target) {
+            return m;
+        }
+        else if (nums[m] < target) {
+            l = m + 1;
+            return searchHelper(nums, target, l, r);
+        }
+        else {
+            r = m - 1;
+            return searchHelper(nums, target, l, r);
+        }
+    }
+
+    public static void main (String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(search(nums, 2));
+    }
+}
